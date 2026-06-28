a7f829af889213be56904af8010e05d392641b5e
AlbertoL1990 <a1990alpalo@gmail.com>
2026-06-27 22:30:00 -0400

    added player classes with coin compositions

---
+++ player.py
@@ -0,0 +1,35 @@
+from coin import Coin
+
+
+class Player:
+    """Represents a player in the Match Coins game."""
+
+    def __init__(self, name):
+        """Initialize the player's name, wallet, and coin."""
+        self.__name = name
+        self.__wallet = 20
+        self.__coin = Coin()
+
+    def toss_coin(self):
+        """Tell the player's coin to toss itself."""
+        self.__coin.toss()
+
+    def get_coin_side(self):
+        """Return the side facing up on the player's coin."""
+        return self.__coin.get_sideup()
+
+    def win_coin(self):
+        """Add one coin to the player's wallet."""
+        self.__wallet += 1
+
+    def lose_coin(self):
+        """Remove one coin from the player's wallet."""
+        self.__wallet -= 1
+
+    def get_wallet(self):
+        """Return the number of coins in the player's wallet."""
+        return self.__wallet
+
+    def get_name(self):
+        """Return the player's name."""
+        return self.__name
